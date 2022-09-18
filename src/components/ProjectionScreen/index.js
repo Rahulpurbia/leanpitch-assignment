dc1970c6ae4bae2db384bfc79bf15d1de74a02b2
RahulPurbia11 <108067767+RahulPurbia11@users.noreply.github.com>
2022-09-18 10:52:54 +0530

    feat: Show content based on menu

--- src/components/ProjectionScreen/index.js
+++ src/components/ProjectionScreen/index.js
@@ -1,19 +1,57 @@
 import React from 'react'
-import { useEffect } from "react"
-import TrainingContent from "../TrainingContent"
+import TrainingContent from "./components/TrainingContent"
+
+import { TRAININGS, WEBINARS, MEETUPS, CONFERENCE, VIDEOS, PSS, PODCASTS, BLOGS, BAS, BAV, ABOUT } from "../../pages/dashboard"
 
 import './style.css'
 
 const ProjectionScreen = ({ hoveredListItem }) => {
 
-    useEffect(() => {
-        console.log(hoveredListItem)
-    }, [hoveredListItem])
+    const getContent = () => {
+        switch (hoveredListItem) {
+            case TRAININGS: {
+                return 'trainings'
+            }
+            case ABOUT: {
+                return 'about'
+            }
+            case WEBINARS: {
+                return 'webinars'
+            }
+            case MEETUPS: {
+                return 'meetups'
+            }
+            case CONFERENCE: {
+                return 'conference'
+            }
+            case VIDEOS: {
+                return 'videos'
+            }
+            case PSS: {
+                return 'pss'
+            }
+            case PODCASTS: {
+                return 'podcasts'
+            }
+            case BLOGS: {
+                return 'blogs'
+            }
+            case BAS: {
+                return 'bas'
+            }
+            case BAV: {
+                return 'bav'
+            }
+            default: {
+                return ""
+            }
+        }
+    }
 
     return (
         <div className="projection-wrapper">
-            <div className="projection-screen">
-                <TrainingContent />
+            <div className="projection-screen" style={{ color: "white" }}>
+                {getContent()}
             </div>
         </div>
 
